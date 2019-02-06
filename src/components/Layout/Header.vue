57d3f6ff7c1e57570ca0c658a01263a48728eadb
vlad17451 <pentium451>
2019-02-06 15:53:34 +0700

    верстка хедера

--- src/components/Layout/Header.vue
+++ src/components/Layout/Header.vue
@@ -2,7 +2,7 @@
   <div>
     <nav class="navbar navbar-expand-lg navbar-light">
       <a class="navbar-brand" href="#">
-        <img src="/src/assets/logo.png" width="250" class="d-inline-block align-top" alt="">
+        <img src="/src/assets/img/logo.png" width="250" class="d-inline-block align-top" alt="">
       </a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
