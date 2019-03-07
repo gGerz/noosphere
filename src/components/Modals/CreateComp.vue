d9243458fdb85466d4bee16a3637483392c49edf
vlad17451 <pentium451>
2019-03-07 17:06:07 +0700

    удаление компитенций

--- src/components/Modals/CreateComp.vue
+++ src/components/Modals/CreateComp.vue
@@ -4,6 +4,7 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLabel1">Добавить компетенцию</h5>
+                    {{ $store.state.userComp }}
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
@@ -18,84 +19,91 @@
                 </div>
                 <div v-show="error">Компетенция уже добавлена</div>
                 <div class="modal-footer justify-content-center">
-
-                        <span class="btn btn-primary btn-shadow" v-on:click="createComp()">
-                            Добавить
-                        </span>
+                    <span class="btn btn-primary btn-shadow" v-on:click="createComp()">
+                        Добавить
+                    </span>
                 </div>
             </div>
         </div>
     </div>
 </template>
 <script>
-    import VueSelect from 'vue-select'
-    import axios from 'axios'
+  import VueSelect from 'vue-select'
+  import axios from 'axios'
 
-    export default {
-        components: {
-            VueSelect
-        },
-        data() {
-            return {
-                error: false,
-                willCreateId: '',
-                title: '',
-                date: '',
-                begin: '',
-                end: '',
-                price: '',
-                about: '',
-                userInfo: '',
-                selectedIndex: '',
-                selectedCard: '',
-                options: '',
-                selected: '',
-                cons: [],
-                photos: [],
-                globalComps: []
-            }
-        },
-        methods: {
-            closeModal() {
-                $('.create_comp_modal').modal('hide');
-            },
+  export default {
+    components: {
+      VueSelect
+    },
+    data() {
+      return {
+        error: false,
+        willCreateId: '',
+        title: '',
+        date: '',
+        begin: '',
+        end: '',
+        price: '',
+        about: '',
+        userInfo: '',
+        selectedIndex: '',
+        selectedCard: '',
+        options: '',
+        selected: '',
+        cons: [],
+        photos: [],
+        globalComps: []
+      }
+    },
+    methods: {
+      closeModal() {
+        $('.create_comp_modal').modal('hide');
+      },
 
-            createComp() {
-                const formData1 = new FormData()
-                console.log('p_id', this.$store.state.userComp.p_user_id)
-                console.log('com_id ', this.selected.com_id)
+      createComp() {
+        const formData1 = new FormData()
+        console.log('p_id', this.$store.state.userComp.p_user_id)
+        console.log('com_id ', this.selected.com_id)
 
-                formData1.set('cp_p_id', this.$store.state.userComp.p_user_id)
-                formData1.set('cp_com_id', this.selected.com_id)
-                axios({
-                    method: 'post',
-                    url: `http://192.168.1.150/noosfera/public_html/api/v1/competences`,
-                    data: formData1
-                })
-                    .then(response => {
-                        console.log('response', response)
-                      window.location.reload()
-                    })
-                    .catch(response => {
-                        console.log(response)
-                    })
-                this.$emit('updateUserInfo')
-            }
-        },
-        mounted() {
-            axios({
-                method: 'get',
-                url: `http://192.168.1.150/noosfera/public_html/api/v1/coms`,
-            })
-                .then((response) => {
-                    console.log("qqqqqqqq",this.$store.state.userComp)
-                    this.globalComps = response.data
-                })
-                .catch((error) => {
-                    console.error(error)
-                })
+        formData1.set('cp_p_id', this.$store.state.userComp.p_user_id)
+        formData1.set('cp_com_id', this.selected.com_id)
+        axios({
+          method: 'post',
+          url: `http://192.168.1.150/noosfera/public_html/api/v1/competences`,
+          data: formData1
+        })
+          .then(response => {
+            console.log('response', response)
+            window.location.reload()
+          })
+          .catch(response => {
+            console.log(response)
+          })
+        this.$emit('updateUserInfo')
+      }
+    },
+    mounted() {
+      axios({
+        method: 'get',
+        url: `http://192.168.1.150/noosfera/public_html/api/v1/coms`,
+      })
+        .then((response) => {
+          // console.log("len1",response.data.length)
+          // console.log("len2",this.$store.state)
+          // this.globalComps = response.data
+          // for (var i = 0; i < response.data.length; i++) {
+          //   console.log('hi')
+          //   for(var j = 0; j < this.$store.state.userComp.cpCom.length; j++) {
+          //     if (1) {//response.data.indexOf($store.state.userComp.cpCom
+          //       console.log('hi1')
+          this.globalComps = response.data
         }
-    }
+    })
+  .catch((error) => {
+    console.error(error)
+  })
+  }
+  }
 </script>
 <style lang="scss" scoped>
     .inputText {
