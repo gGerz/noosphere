d22098783389e148c23903731e1cf11b8c3a1e9a
vlad17451 <pentium451>
2019-03-07 17:07:05 +0700

    удаление компитенций3

--- src/components/Modals/CreateComp.vue
+++ src/components/Modals/CreateComp.vue
@@ -4,7 +4,6 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLabel1">Добавить компетенцию</h5>
-                    {{ $store.state.userComp }}
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
