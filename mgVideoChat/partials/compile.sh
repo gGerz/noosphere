7bb74415dd3503ad87dd1be9815995ab514663d2
vlad17451 <pentium451>
2019-04-01 15:31:18 +0700

    а

--- mgVideoChat/partials/compile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-cat mgVideoChatHeader.js mgRtc.js mgFileHelper.js mgNotifications.js mgDesktopShare.js mgVideoChatUtils.js mgVideoChatRtcEvents.js mgVideoChatUI.js mgVideoChatBase.js > ../mgVideoChat.js
--- mgVideoChat/partials/mgVideoChatHeader.js
+++
@@ -1,6 +0,0 @@
-/**
- * mgVideoChat
- *
- * @version 1.13.2
- * @copyright magnoliyan
- */
